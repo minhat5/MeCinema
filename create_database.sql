e16eebca620c4ef1d8c9187fb24102ce9ad7c485
minhat5 <23110265@student.hcmute.edu.vn>
2026-06-17 13:49:33 +0700

    add create database in mysql query

--- create_database.sql
+++ create_database.sql
@@ -4,6 +4,8 @@
 -- ------------------------------------------------------
 -- Server version	9.4.0
 
+CREATE DATABASE cinema_booking_db;
+USE cinema_booking_db;
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
